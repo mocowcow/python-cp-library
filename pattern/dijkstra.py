1d4332ffbd75f66cbe44bd14bfecabf8559d9346
ariduki <ariduki@gmail.com>
2022-03-15 22:23:04 +0800

    add

--- pattern/dijkstra.py
+++ pattern/dijkstra.py
@@ -16,7 +16,7 @@ def dijkstra(g, n, src):  # graph with n nodes start from src
     return table
 
 
-# 6 nodes DAG
+# 6 nodes directed graph
 edges = [[0, 2, 2], [0, 5, 6], [1, 0, 3], [1, 4, 5], [2, 1, 1], [2, 3, 3], [2, 3, 4], [3, 4, 2]]
 n = 6
 # build graph
