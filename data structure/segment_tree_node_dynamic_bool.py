a5698b989f2a6b01fd1b401882a77aafb9ce8d42
ariduki <ariduki@gmail.com>
2022-04-08 20:46:50 +0800

    rename

--- data structure/segment_tree_node_dynamic_bool.py
+++ data structure/segment_tree_node_dynamic_bool.py
@@ -1,9 +1,9 @@
 
 class Node:
-    def __init__(self, L, R, book):
+    def __init__(self, L, R, used):
         self.L = L
         self.R = R
-        self.book = book
+        self.used = used
         self.left = self.right = None
 
 
@@ -18,39 +18,37 @@ class SegmentTree:
         if i > node.R or j < node.L:  # out of range
             return False
         if i <= node.L and j >= node.R:  # fully covered
-            return node.book
+            return node.used
         if not node.left:
-            return node.book
+            return node.used
         return self._q(node.right, i, j) or self._q(node.left, i, j)
 
     def update(self, i, j):
         self._u(self.root, i, j)
 
     def _u(self, node, i, j):
-        # print(node.L, node.R)
         if i > node.R or j < node.L:  # out of range
             return
         if i <= node.L and j >= node.R:  # fully covered
-            # print(node.L, node.R, 'is true')
-            node.book = True
+            node.used = True
             node.left = node.right = None
             return
         M = (node.L+node.R)//2
         if not node.left:
-            node.left = Node(node.L, M, node.book)
-            node.right = Node(M+1, node.R, node.book)
+            node.left = Node(node.L, M, node.used)
+            node.right = Node(M+1, node.R, node.used)
         if M >= i:
             self._u(node.left, i, j)
         if M < j:
             self._u(node.right, i, j)
-        node.book = node.left.book or node.right.book
+        node.used = node.left.used or node.right.used
 
 
-def book(i, j):
-    booked = st.query(i, j-1)
-    if not booked:
+def f(i, j):
+    used = st.query(i, j-1)
+    if not used:
         st.update(i, j-1)
-        print('booked', i, j-1)
+        print('set', i, j-1, 'to True')
     else:
         print('!!!   FAILED', i, j-1)
 
@@ -58,5 +56,5 @@ def book(i, j):
 st = SegmentTree(0, 10**9+5)
 
 test = [[377791053, 395853223], [363789408, 379405233]]
-for a, b in test[:]:
-    book(a, b)
+for a, b in test:
+    f(a, b)
