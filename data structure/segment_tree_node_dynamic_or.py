10ded21e12a486272d18eded0d1ae770f55f9a58
ariduki <ariduki@gmail.com>
2022-04-08 21:29:05 +0800

    add parameter for update method

--- data structure/segment_tree_node_dynamic_or.py
+++ data structure/segment_tree_node_dynamic_or.py
@@ -23,14 +23,14 @@ class SegmentTree:
             return node.used
         return self._q(node.right, i, j) or self._q(node.left, i, j)
 
-    def update(self, i, j):
-        self._u(self.root, i, j)
+    def update(self, i, j, used):
+        self._u(self.root, i, j, used)
 
-    def _u(self, node, i, j):
+    def _u(self, node, i, j, used):
         if i > node.R or j < node.L:  # out of range
             return
         if i <= node.L and j >= node.R:  # fully covered
-            node.used = True
+            node.used = used
             node.left = node.right = None
             return
         M = (node.L+node.R)//2
@@ -38,9 +38,9 @@ class SegmentTree:
             node.left = Node(node.L, M, node.used)
             node.right = Node(M+1, node.R, node.used)
         if M >= i:
-            self._u(node.left, i, j)
+            self._u(node.left, i, j, used)
         if M < j:
-            self._u(node.right, i, j)
+            self._u(node.right, i, j, used)
         node.used = node.left.used or node.right.used
 
 
