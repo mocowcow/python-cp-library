d5540a1da68add61a90620b54af7819a1486b187
ariduki <ariduki@gmail.com>
2022-09-18 15:11:24 +0800

    upd sorting

--- pattern/basic_sorting.py
+++ pattern/basic_sorting.py
@@ -1,3 +1,8 @@
+import random
+
+
+def swap(nums, a, b):
+    nums[a], nums[b] = nums[b], nums[a]
 
 
 def selection(src):
@@ -12,7 +17,7 @@ def selection(src):
         for j in range(i+1, N):
             if a[j] < a[mn]:
                 mn = j
-        a[i], a[mn] = a[mn], a[i]
+        swap(a, i, mn)
         print(a)
     return a
 
@@ -24,11 +29,10 @@ def bubble(src):
     print('origin=', src)
     a = src[:]
     N = len(a)
-    for i in range(N-1, 0, -1):
-        for j in range(i):
-            if a[j] > a[j+1]:
-                a[j], a[j+1] = a[j+1], a[j]
-        print(a)
+    for i in range(N):
+        for j in range(1, N-i):
+            if a[j-1] > a[j]:
+                swap(a, j-1, j)
     return a
 
 
@@ -51,8 +55,40 @@ def insertion(src):
     return a
 
 
+def quick(src):
+    print('quick sort')
+    print('origin=', src)
+    a = src[:]
+    N = len(a)
+
+    def partition(nums, left, right):
+        idx = random.randint(left, right)
+        pivot = nums[idx]
+        # move pivot to right most
+        swap(nums, idx, right)
+        # partition
+        i = left
+        for j in range(left, right):
+            if nums[j] < pivot:
+                swap(nums, i, j)
+                i += 1
+        # move pivot to middle
+        swap(nums, i, right)
+        return i
+
+    def qs(nums, left, right):
+        if left < right:
+            idx = partition(nums, left, right)
+            qs(nums, left, idx-1)
+            qs(nums, idx+1, right)
+
+    qs(a, 0, N-1)
+    return a
+
+
 src = [5555, 456, 8, 7, 3, 21, 5, 6, 0, -5, 8, 56, 777, 88]
-src=[3,2,1,0,-1]
+src = [3, 2, 1, 0, -1]
 # assert sorted(src) == selection(src)
-assert sorted(src) == bubble(src)
+# assert sorted(src) == bubble(src)
 # assert sorted(src) == insertion(src)
+# assert sorted(src) == quick(src)
