746e0f4389ca12b66d77d4669c1407c7b28cc05e
ariduki <ariduki@gmail.com>
2023-06-05 14:29:54 +0800

    upd slope fraction

--- pattern/slope.py
+++ pattern/slope.py
@@ -7,13 +7,12 @@ def gcd(a, b):
 
 # 傳入dy,dx
 # 回傳dy/dx最簡分數
-def fraction_slope(a, b):
-    _gcd = gcd(a, b)
-    return (a//_gcd, b//_gcd)
+def fraction_slope(dx, dy):
+    _gcd = gcd(dx, dy)
+    return (dx//_gcd, dy//_gcd)
 
 
 # 判斷三點共線
 # (b[1]-a[1]) / (b[0]-a[0]) == (c[1]-b[1]) / (c[0]-b[0])
 def same_line(a, b, c):
     return (b[1]-a[1])*(c[0]-b[0]) == (c[1]-b[1])*(b[0]-a[0])
-
