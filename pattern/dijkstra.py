128ec8cb3316a7e39b31971be9b701a806a46bcc
ariduki <ariduki@gmail.com>
2023-06-09 23:11:52 +0800

    upd dijkstra with heap

--- pattern/dijkstra.py
+++ pattern/dijkstra.py
@@ -1,28 +1,38 @@
 from collections import defaultdict
 from heapq import heappop, heappush
-import math
+from math import inf
 
 
-def dijkstra(g, n, src):  # graph with n nodes start from src
-    table = [math.inf]*(n)
+# find all min distance from src to other nodes
+# in n-nodes graph g
+def dijkstra(g, n, src):
+    dis = [inf]*(n)
+    dis[src] = 0
     heap = [(0, src)]
     while heap:
-        time, curr = heappop(heap)
-        if time < table[curr]:
-            table[curr] = time
-            for adj, cost in g[curr]:
-                heappush(heap, (time+cost, adj))
+        cost, curr = heappop(heap)
+        if cost > dis[curr]:
+            continue
+        dis[curr] = cost
+        for adj, c in g[curr]:
+            new_cost = cost+c
+            if new_cost < dis[adj]: # important pruning
+                dis[adj] = new_cost
+                heappush(heap, (new_cost, adj))
 
-    return table
+    return dis
 
 
 # 6 nodes directed graph
-edges = [[0, 2, 2], [0, 5, 6], [1, 0, 3], [1, 4, 5], [2, 1, 1], [2, 3, 3], [2, 3, 4], [3, 4, 2]]
+edges = [[0, 2, 2], [0, 5, 6], [1, 0, 3], [1, 4, 5],
+         [2, 1, 1], [2, 3, 3], [2, 3, 4], [3, 4, 2]]
 n = 6
+
 # build graph
 graph = defaultdict(list)
 for a, b, cost in edges:
     graph[a].append((b, cost))
+
 # min distance from src=0 to all nodes
 src = 0
 t = dijkstra(graph, n, src)
