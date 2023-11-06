0eb0f8e7696ff1cc67126fca47f98d1a9f91ba27
ariduki <ariduki@gmail.com>
2023-11-07 00:03:36 +0800

    refactor segment tree

--- data structure/segment tree/segment_tree.py
+++ data structure/segment tree/segment_tree.py
@@ -18,9 +18,11 @@ update(1, 0, N-1, i, val)
 """
 
 
-# 初始化線段樹
-# 若無初始值則不需執行
 def build(id, L, R):
+    """
+    初始化線段樹
+    若無初始值則不需執行
+    """
     if L == R:  # 到達葉節點
         tree[id] = init[L]
         return
@@ -30,9 +32,11 @@ def build(id, L, R):
     tree[id] = tree[id*2]+tree[id*2+1]  # 以左右子樹更新答案
 
 
-# 區間查詢
-# 回傳[i, j]的總和
 def query(id, L, R, i, j):
+    """
+    區間查詢
+    回傳[i, j]的總和
+    """
     if i <= L and R <= j:  # 當前區間目標範圍包含
         return tree[id]
     ans = 0
@@ -44,9 +48,11 @@ def query(id, L, R, i, j):
     return ans
 
 
-# 單點更新
-# 對索引i增加val
 def update(id, L, R, i, val):
+    """
+    單點更新
+    對索引i增加val
+    """
     if L == R:  # 當前區間目標範圍包含
         tree[id] += val
         return
