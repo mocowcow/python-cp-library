4132dad3f09e666fbad74faa8c72eee16245c27b
ariduki <ariduki@gmail.com>
2023-12-18 23:04:16 +0800

    add median

---
+++ pattern/math/median.py
@@ -0,0 +1,25 @@
+
+# 在有序陣列找中位數
+# 左中位數索引為(N-1)/2
+# 又中位數索引為N/2
+nums = []
+
+# example:
+# l = (N-1)/2 = 1, nums[l] = 2
+# r = N/2 = 2, nums[r]= 3
+# median = (2+3)/2 = 2.5
+
+# example2:
+# nums = [1,2,3], N = 3
+# l = (N-1)/2 = 1, nums[l] = 2
+# r = N/2 = 1, nums[r] = 2
+# median = (2+3)/2 = 2
+
+N = len(nums)
+l = (N-1)//2
+r = N//2
+median = (nums[l]+nums[r])//2
+
+# 簡短版本
+N = len(nums)
+median = (nums[(N-1)//2]+nums[N//2])//2
