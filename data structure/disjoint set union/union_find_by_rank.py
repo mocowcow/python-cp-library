46eb3371812749f71b26dc0a2fb4e7c6db2ffe91
ariduki <ariduki@gmail.com>
2024-02-01 19:57:06 +0800

    upd union find

---
+++ data structure/disjoint set union/union_find_by_rank.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [0]*n
+        self.rank = [0]*n  # 可以是節點數或高度，此為高度
+        for i in range(n):
+            self.parent[i] = i
+
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px == py:
+            return
+
+        if self.rank[px] > self.rank[py]:  # 限制 px <= py
+            px, py = py, px
+
+        self.parent[px] = py  # 將小的合併到大的
+        if self.rank[px] == self.rank[py]:
+            self.rank[py] += 1
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
