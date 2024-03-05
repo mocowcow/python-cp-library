e27d4e2e4741454d5a5e560d6aaedde28236bdf1
ariduki <ariduki@gmail.com>
2024-03-05 13:30:18 +0800

    refactor

--- pattern/prefix_sum.py
+++ pattern/prefix_sum.py
@@ -4,27 +4,17 @@ from itertools import accumulate
 def prefix_sum(nums):
     N = len(nums)
     psum = [0]*(N+1)
-    for i, n in enumerate(nums):
-        psum[i+1] = psum[i]+n
+    for i, x in enumerate(nums):
+        psum[i+1] = psum[i] + x
     return psum
 
 
 def prefix_sum_with_append(nums):
     psum = [0]
-    for n in nums:
-        psum.append(psum[-1]+n)
+    for x in nums:
+        psum.append(psum[-1] + x)
     return psum
 
 
 def prefix_sum_with_accumulate(nums):
-    return [0]+list(accumulate(nums))
-
-
-psum = prefix_sum([1, 2, 3, 4, 6, 7, 10])
-print(psum)
-left, right = 3, 5  # nums[3]連加至nums[5]
-print(psum[right+1]-psum[left])  # 17
-left, right = 0, 2  # nums[0]連加至nums[2]
-print(psum[right+1]-psum[left])  # 6
-left, right = 1, 1  # nums[1]
-print(psum[right+1]-psum[left])  # 2
+    return list(accumulate(nums, initial=0))
--- pattern/prefix_sum_2D.py
+++ pattern/prefix_sum_2D.py
@@ -2,14 +2,16 @@ class PrefixSum2D:
 
     def __init__(self, matrix):
         M, N = len(matrix), len(matrix[0])
-        self.psum = [[0]*(N+1) for _ in range(M+1)]
+        ps = self.ps = [[0]*(N+1) for _ in range(M+1)]
         for r in range(M):
             for c in range(N):
-                self.psum[r+1][c+1] = self.psum[r][c+1] + \
-                    self.psum[r+1][c]-self.psum[r][c]+matrix[r][c]
+                ps[r+1][c+1] = \
+                    ps[r][c+1] + ps[r+1][c] - \
+                    ps[r][c] + matrix[r][c]
 
-    def rangeSum(self, r1, c1, r2, c2):
-        return self.psum[r2+1][c2+1]-self.psum[r2+1][c1]-self.psum[r1][c2+1]+self.psum[r1][c1]
+    def range_sum(self, r1, c1, r2, c2):
+        ps = self.ps
+        return ps[r2+1][c2+1] - ps[r2+1][c1] - ps[r1][c2+1] + ps[r1][c1]
 
 
 mat = [[1, 2, 3],
@@ -17,9 +19,9 @@ mat = [[1, 2, 3],
        [7, 8, 9]]
 
 ps2d = PrefixSum2D(mat)
-assert ps2d.rangeSum(0, 0, 2, 2) == 45
-assert ps2d.rangeSum(0, 0, 1, 2) == 21
-assert ps2d.rangeSum(0, 1, 2, 2) == 33
-assert ps2d.rangeSum(2, 1, 2, 2) == 17
-assert ps2d.rangeSum(1, 1, 1, 2) == 11
-assert ps2d.rangeSum(2, 2, 2, 2) == 9
+assert ps2d.range_sum(0, 0, 2, 2) == 45
+assert ps2d.range_sum(0, 0, 1, 2) == 21
+assert ps2d.range_sum(0, 1, 2, 2) == 33
+assert ps2d.range_sum(2, 1, 2, 2) == 17
+assert ps2d.range_sum(1, 1, 1, 2) == 11
+assert ps2d.range_sum(2, 2, 2, 2) == 9
