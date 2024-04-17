e4d0b8f4556ffde8cb54308547e040889760e256
ariduki <ariduki@gmail.com>
2024-04-17 23:27:13 +0800

    refactor

--- pattern/math/prime.py
+++ pattern/math/prime.py
@@ -2,12 +2,12 @@
 # Sieve of Eratosthenes
 # O(n log (log n))
 def get_prime(n):
-    sieve = [True]*(n+1)
+    sieve = [True] * (n + 1)
     prime = []
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         if sieve[i]:
             prime.append(i)
-            for j in range(i*i, n+1, i):
+            for j in range(i * i, n + 1, i):
                 sieve[j] = False
     return prime
 
@@ -15,11 +15,11 @@ def get_prime(n):
 # build prime table
 # O(sqrt(n))
 def prime_table(n):
-    sieve = [True]*(n+1)
+    sieve = [True] * (n + 1)
     sieve[0] = sieve[1] = False
-    for i in range(2, int(n**0.5)+1):
+    for i in range(2, int(n ** 0.5) + 1):
         if sieve[i]:
-            for j in range(i*i, n+1, i):
+            for j in range(i * i, n + 1, i):
                 sieve[j] = False
     return sieve
 
@@ -29,7 +29,7 @@ def prime_table(n):
 def prime_fact(n):
     fact = []
     p = 2
-    while p*p <= n:
+    while p * p <= n:
         while n % p == 0:
             fact.append(p)
             n //= p
@@ -42,9 +42,7 @@ def prime_fact(n):
 # check is prime or not
 # O(sqrt(n))
 def is_prime(n):
-    if n == 1:
-        return False
-    for i in range(2, int(n**0.5)+1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
-    return True
+    return n >= 2
