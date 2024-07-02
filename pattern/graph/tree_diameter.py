9a5ddf6d46068ef85a06b28c853feeb1ccf3f0e1
ariduki <ariduki@gmail.com>
2024-07-03 00:00:19 +0800

    add bfs

--- pattern/graph/tree_diameter.py
+++ pattern/graph/tree_diameter.py
@@ -24,3 +24,36 @@ def diameter_dfs(edges):
     mx_dist = -1
     dfs(farthest, -1, 0)
     return mx_dist
+
+
+def diameter_bfs(edges):
+    N = len(edges) + 1
+    g = [[] for _ in range(N)]
+    for a, b in edges:
+        g[a].append(b)
+        g[b].append(a)
+
+    def bfs(start):
+        dist = [-1] * N
+        dist[start] = 0
+        q = [start]
+        while q:
+            q2 = []
+            for i in q:
+                for j in g[i]:
+                    if dist[j] == -1:
+                        dist[j] = dist[i] + 1
+                        q2.append(j)
+            q = q2
+
+        farthest = None
+        mx_dist = -1
+        for i, d in enumerate(dist):
+            if d > mx_dist:
+                mx_dist = d
+                farthest = i
+        return farthest, mx_dist
+
+    u, dist = bfs(0)
+    v, dist = bfs(u)
+    return dist
