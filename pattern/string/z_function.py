7d64affa320b4b862d6c9018e5ba2228aa29729a
ariduki <ariduki@gmail.com>
2024-10-12 21:54:30 +0800

    add link

--- pattern/string/z_function.py
+++ pattern/string/z_function.py
@@ -1,4 +1,21 @@
 
+# LCP (Longest Common prefix)：最長公共前綴
+# s[i..]：以 s[i] 開頭的後綴
+# 定義 z[i] = LCP(s, s[i..])
+# z[0] 即字串 s 與自身匹配，不具意義。特別約定 z[i] = 0
+
+
+# 文本與後綴求 LCP
+# LC2223 https://leetcode.com/problems/sum-of-scores-of-built-strings/
+# LC3031 https://leetcode.com/problems/minimum-time-to-revert-word-to-initial-state-ii/
+# LC3045 https://leetcode.com/problems/count-prefix-and-suffix-pairs-ii/
+
+
+# 文本 s 與模式 p 串接，做字串查找
+# LC3008 https://leetcode.com/problems/find-beautiful-indices-in-the-given-array-ii/
+# LC3034 https://leetcode.com/problems/number-of-subarrays-that-match-a-pattern-ii/
+# LC3303 https://leetcode.com/problems/find-the-occurrence-of-first-almost-equal-substring/
+
 
 def z_function(s):
     N = len(s)
