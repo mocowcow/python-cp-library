2baf9c4aa13b9963be0c32ce8784543827cca459
ariduki <ariduki@gmail.com>
2025-05-25 23:47:02 +0800

    add LC playground

---
+++ LC_playground.py
@@ -0,0 +1,13 @@
+from math import inf, factorial, comb
+import operator
+from operator import itemgetter
+from bisect import *
+from collections import *
+from heapq import *
+from typing import *
+from itertools import *
+from functools import *
+from string import ascii_lowercase
+from sortedcontainers import SortedList as SL
+
+MOD = 10 ** 9 + 7
