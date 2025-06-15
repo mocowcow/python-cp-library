a422414f0850d7a8559f4e443fe0d3d388a69900
ariduki <ariduki@gmail.com>
2025-06-15 10:25:32 +0800

    tweak

--- LC_playground.py
+++ LC_playground.py
@@ -1,4 +1,4 @@
-from math import inf, factorial, comb
+from math import inf, factorial, comb, gcd, lcm
 import operator
 from operator import itemgetter
 from bisect import *
@@ -11,3 +11,4 @@ from string import ascii_lowercase
 from sortedcontainers import SortedList as SL
 
 MOD = 10 ** 9 + 7
+MX = 10 ** 5 + 5
